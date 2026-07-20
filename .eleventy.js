91d9f6f2df1ba1115e819cd3d4504a3a0cab93f3
netless-ww <59403808+netless-ww@users.noreply.github.com>
2026-07-20 18:44:59 +1000

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,12 @@
+const yaml = require("js-yaml");
+const MarkdownIt = require("markdown-it");
+
+const md = new MarkdownIt({ html: false, breaks: true, linkify: true });
+
 module.exports = function (eleventyConfig) {
+  eleventyConfig.addDataExtension("yaml", (contents) => yaml.load(contents));
+  eleventyConfig.addFilter("md", (content) => md.renderInline(content || ""));
+
   eleventyConfig.addPassthroughCopy({ "src/assets": "assets" });
   eleventyConfig.addPassthroughCopy({ "admin": "admin" });
 
